a6753eefce4f0c50b7b4eadc99b24eb628d94b13
Gugutataa <111111608+Gugutataa@users.noreply.github.com>
2022-08-19 19:42:24 +0200

    finally fucking works

--- signout.py
+++ signout.py
@@ -4,6 +4,7 @@ timer = input("Enter time until the signout happens:")
 import win32gui, win32con
 the_program_to_hide = win32gui.GetForegroundWindow()
 win32gui.ShowWindow(the_program_to_hide , win32con.SW_HIDE)
+timer = float(timer)
 time.sleep(timer)
 pyautogui.hotkey("win", "m")
 pyautogui.hotkey("alt", "f4")
